dcd89139d9b65afff0e4aa92776d8b9debdd40b0
Administrador <Administrador@172.16.17.105>
2018-04-17 15:08:57 -0600

    esta es la parte 1.0.4 del proyecto

--- ismaelMenjivar/src/ismaelmenjivar/IsmaelMenjivar.java
+++ ismaelMenjivar/src/ismaelmenjivar/IsmaelMenjivar.java
@@ -12,25 +12,32 @@ public class IsmaelMenjivar {
         /* Scanner leer =new Scanner(system.in);
         */
         int cantidad;
-        int producto;
+        String producto;
         double precio;
-        double total;
+        int total;
         double iva;
         int opcion;
+        int cantidad1;
+        
         
         opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "1) Agregar producto, precio y cantidad que lleva \n 2) calcular Total \n 3) Salir", "menu"));
            switch(opcion){
                case 1:
                       
-                   cantidad=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese la cantidad de productos precio y calcular el total"));
+                   cantidad=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese la cantidad de los productos , el nombre del producto y su precio "));
                     
                    for (int i=0; i<cantidad;i++)
-                   producto=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese los productos con su precio y el total que lleva"));
+                   producto=JOptionPane.showInputDialog(null,"ingrese los productos  que lleva");
+                   precio=Double.parseDouble(JOptionPane.showInputDialog(null," Agregar precio"));
+                   cantidad1=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese cuanto lleva"));
                    break;
                case 2:
-               
+              /* total=cantidad1*precio/0.13;
                    break;
-        
+                */   
+               case 3:
+                 
+                   break;
         
         
         
