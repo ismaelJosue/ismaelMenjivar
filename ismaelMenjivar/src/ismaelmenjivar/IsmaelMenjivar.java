ed3606f38ed9cd4e6a1daec9aef3ae12289cba5e
Administrador <Administrador@172.16.17.105>
2018-04-17 14:09:20 -0600

    esta es la parte uno del proyecto

---
+++ ismaelMenjivar/src/ismaelmenjivar/IsmaelMenjivar.java
@@ -0,0 +1,17 @@
+
+package ismaelmenjivar;
+public class IsmaelMenjivar {
+
+    
+     
+    public static void main(String[] args) {
+        
+        /* Scanner leer =new Scanner(system.in);
+        */
+        int cantidad;
+        double precio;
+        double total;
+        
+    }
+    
+}
