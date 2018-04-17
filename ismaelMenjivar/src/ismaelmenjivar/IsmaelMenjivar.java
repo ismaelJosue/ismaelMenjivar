5605c6b8c75cf75a249707c37cd94de5fbad2c34
Administrador <Administrador@172.16.17.105>
2018-04-17 14:31:38 -0600

    esta es la parte 1.0.1 del proyecto

--- ismaelMenjivar/src/ismaelmenjivar/IsmaelMenjivar.java
+++ ismaelMenjivar/src/ismaelmenjivar/IsmaelMenjivar.java
@@ -1,5 +1,8 @@
 
 package ismaelmenjivar;
+
+import javax.swing.JOptionPane;
+
 public class IsmaelMenjivar {
 
     
@@ -9,9 +12,25 @@ public class IsmaelMenjivar {
         /* Scanner leer =new Scanner(system.in);
         */
         int cantidad;
+        char producto;
         double precio;
         double total;
+        double iva;
+        int opcion;
         
-    }
+        opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "1) Agregar producto \n 2) calcular Total \n 3) Salir", "menu"));
+           
+        cantidad=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese la cantidad de productos"));
+        
+        for (int i=0; i<cantidad;i++)
+        {
+        producto=char.parseChar(JOptionPane.showMessageDialog(null,"producto:"+));
+        
+        
+        
+        }
+        
+                
+            }
     
 }
